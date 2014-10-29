175786293b6d105ee4a35b0aa74a75bae805ffea
Jeremiah Guile <holdthefather@gmail.com>
2014-10-28 23:23:51 -0600

    Jeremiah's first commitment.

---
+++ School Project #1/Variables/Variables/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Jeremiah. What door do you want to open (1,2, or 3)?");
+            try
+            {
+                int x = int.Parse(Console.ReadLine());
+                if (x == 1)
+                {
+                    
+                    Console.WriteLine("You won a car! Do you want to open the door? (Yes or No)");
+                    string y = (Console.ReadLine()).ToLower();
+                    if (y == "yes")
+                        Console.WriteLine("You died in a massive explosion.");
+                    else if (y == "no")
+                        Console.WriteLine("You survived another day to open more \"doors\".");
+                    else
+                        throw new InvalidOperationException();
+ 
+
+                }
+                else if (x == 2)
+                {
+                    Console.WriteLine("You won a boat!");
+                }
+                else if (x == 3)
+                {
+                    Console.WriteLine("You die.");
+                }
+                else
+                {
+                    Console.WriteLine("Pay attention to instructions, you moron.");
+                }
+            }
+            catch(InvalidOperationException ioe)
+            {
+                Console.WriteLine("invalid choice" + ioe.Message);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("you fucking idiot. You're not playing the game right. Im leaving. " + ex.Message);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
